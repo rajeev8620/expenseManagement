9ccae31958c7967a7b0472981d1ce051b43a8bc1
rajeev8620 <kumarrajeev.191@gmail.com>
2019-07-31 15:13:30 +0530

    Updated side nav bar

---
+++ gitcommit.sh
@@ -0,0 +1,7 @@
+git add .
+git commit -m "updated sidenav bar display color"
+git remote add origin "https://github.com/rajeev8620/expenseManagement"
+git commit -a
+git remote -v
+git push -f
+git push origin master
