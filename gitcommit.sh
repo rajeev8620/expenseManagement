8b61881d1f034625d0ddbd4a0dc7185cae2f64d1
rajeev8620 <kumarrajeev.191@gmail.com>
2019-08-01 12:10:41 +0530

    Removed all unwanted files

--- gitcommit.sh
+++
@@ -1,7 +0,0 @@
-git add .
-git commit -m "updated sidenav bar display color"
-git remote add origin "https://github.com/rajeev8620/expenseManagement"
-git commit -a
-git remote -v
-git push -f
-git push origin master
